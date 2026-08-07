a72021fad3caefa964346409f19a97d5588dcab8
riven <rivensinn@gmail.com>
2026-08-07 22:54:31 +0700

    Remove GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -3,24 +3,24 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Home from './pages/Home.jsx'
-import { createHashRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import FrontEndJourney from './pages/FrontEndJourney.jsx'
 import Malice from './pages/Malice.jsx'
 import HolyMother from './pages/HolyMother.jsx'
 import LawsOfPower from './pages/LawsOfPower.jsx'
 import GirlInTheDark from './pages/GirlInTheDark.jsx'
 
-const router = createHashRouter([
+const router = createBrowserRouter([
   {
     path:"/",
     element: <App />,
     children: [
-      {path: "/", element: <Home /> },
-      {path: "/My-Frontend-Dev-Learning-Journey", element: <FrontEndJourney /> },
-      {path: "/Malice", element: <Malice /> },
-      {path: "/HolyMother", element: <HolyMother /> },
-      {path: "/48-Laws-Of-Power", element: <LawsOfPower /> },
-      {path: "/GirlInTheDark", element: <GirlInTheDark /> },
+      {index: true, element: <Home /> },
+      {path: "My-Frontend-Dev-Learning-Journey", element: <FrontEndJourney /> },
+      {path: "Malice", element: <Malice /> },
+      {path: "HolyMother", element: <HolyMother /> },
+      {path: "48-Laws-Of-Power", element: <LawsOfPower /> },
+      {path: "GirlInTheDark", element: <GirlInTheDark /> },
     ]
   }
   ]
